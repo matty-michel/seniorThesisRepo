302ecaf64c55fd05ea0df900a9dd0be7ac048b88
matty-michel <mmichel@carthage.edu>
2025-09-22 13:21:37 -0500

    worked on player controller

---
+++ senior_thesis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRigidBody;
+    private float _horizontalInput;
+    private bool _isOnGround = true;
+    public float speed;
+    public float jumpForce;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            //applies upward force immediately
+            _playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //player is off the ground
+            _isOnGround = false;
+        }
+        
+        //character moves back and forth
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * _horizontalInput * Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //player is on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isOnGround = true;
+        }
+    }
+}
