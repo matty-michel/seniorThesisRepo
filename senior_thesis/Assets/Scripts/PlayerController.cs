a56aa185ae688eff00823a384e975b888683a5e2
matty-michel <mmichel@carthage.edu>
2025-09-24 22:04:44 -0500

    configured unity project for 2D

--- senior_thesis/Assets/Scripts/PlayerController.cs
+++ senior_thesis/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody _playerRigidBody;
+    private Rigidbody2D _playerRigidBody;
     private float _horizontalInput;
     private bool _isOnGround = true;
     public float speed;
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _playerRigidBody = GetComponent<Rigidbody>();
+        _playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
         {
             //applies upward force immediately
-            _playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            _playerRigidBody.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
             //player is off the ground
             _isOnGround = false;
         }
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * _horizontalInput * Time.deltaTime * speed);
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         //player is on the ground
         if (collision.gameObject.CompareTag("Ground"))
