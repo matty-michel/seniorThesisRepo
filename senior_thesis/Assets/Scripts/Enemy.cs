198923e78e7ad1b8dc00d2b6a7c44bc424bf5a88
matty-michel <mmichel@carthage.edu>
2025-10-03 16:28:27 -0500

    enemy follows player when within attack radius

--- senior_thesis/Assets/Scripts/Enemy.cs
+++ senior_thesis/Assets/Scripts/Enemy.cs
@@ -4,6 +4,10 @@ public class Enemy : MonoBehaviour
 {
     private Rigidbody2D _enemyRigidbody;
     private GameObject _player;
+    private bool _playerInRange;
+    //private float _detectionRange = 5f;
+    //private float _attackRange = 1f;
+    //private bool aggro = false;
     public float speed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,7 +22,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_playerInRange)
+        {
+            MoveTowardsPlayer();
+        }
+        /*if (InDetectionRange())
+        {
+            aggro = true;
+            MoveTowardsPlayer();
+        }*/
     }
 
     void MoveTowardsPlayer()
@@ -29,11 +41,27 @@ public class Enemy : MonoBehaviour
         _enemyRigidbody.AddForce(moveDirection * speed);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.CompareTag("Player"))
+        //player is in aggro range of enemy
+        if (collision.CompareTag("Player"))
         {
-			MoveTowardsPlayer();
+            _playerInRange = true;
         }
     }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _playerInRange = false;
+        }
+    }
+
+    /*bool InDetectionRange()
+    {
+        float playerDistanceSqr = (_player.transform.position - transform.position).sqrMagnitude;
+        //returns true when player is within detection range
+        return playerDistanceSqr <= (_detectionRange * _detectionRange);
+    }*/
 }
