2b09924878096180ab918bb68749f833f3a11b8e
matty-michel <mmichel@carthage.edu>
2025-10-05 17:05:08 -0500

    enemy patrols between two points
    - enemy stops patrolling when attacking the player
    - AttackPlayer script removed, attack functionality added to Enemy script
    - removed chase functionality from Enemy script for the time being

--- senior_thesis/Assets/Scripts/Enemy.cs
+++ senior_thesis/Assets/Scripts/Enemy.cs
@@ -2,53 +2,86 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody2D _enemyRigidbody;
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+    //enemy can attack immediately
+    private float _cooldownTimer = Mathf.Infinity;
+    private Health _playerHealth;
     private GameObject _player;
     private bool _playerInRange;
-    public float speed = 1f;
+    private EnemyPatrol _enemyPatrol;
+    
+    //private Rigidbody2D _enemyRigidbody;
+    //public float speed = 1f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        //getting enemy rigidbody
-        _enemyRigidbody = GetComponent<Rigidbody2D>();
         //getting player
         _player = GameObject.Find("Player");
+        //getting health script of player
+        _playerHealth = _player.GetComponent<Health>();
+        //getting enemy patrol script of parent patrol object
+        _enemyPatrol = GetComponentInParent<EnemyPatrol>();
+        //getting enemy rigidbody
+        //_enemyRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //updating cooldown
+        _cooldownTimer += Time.deltaTime;
+        
         if (_playerInRange)
         {
-            MoveTowardsPlayer();
+            if (_cooldownTimer >= attackCooldown)
+            {
+                //attack
+                _cooldownTimer = 0;
+                DamagePlayer();
+            }
+        }
+
+        //enemy only patrols when not attacking the player
+        if (_enemyPatrol != null)
+        {
+            _enemyPatrol.enabled = !_playerInRange;
+        }
+    }
+    
+    void DamagePlayer()
+    {
+        //damage player when in range
+        if (_playerInRange)
+        {
+            _playerHealth.TakeDamage(damage);
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        //player is in aggro range of enemy
+        //player is in attack range of enemy
         if (collision.CompareTag("Player"))
         {
             _playerInRange = true;
-            Debug.Log("Player In Range");
         }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
+        //player is out of attack range of enemy
         if (collision.CompareTag("Player"))
         {
             _playerInRange = false;
-            Debug.Log("Player Out Of Range");
         }
     }
     
-    void MoveTowardsPlayer()
+    /*void MoveTowardsPlayer()
     {
         //getting the force direction by subtracting enemy pos from player pos
         //normalized keeps the force the same regardless of distance
         Vector2 moveDirection = (_player.transform.position - transform.position).normalized;
         _enemyRigidbody.AddForce(moveDirection * speed);
-    }
+    }*/
 }
--- senior_thesis/Assets/Scripts/EnemyPatrol.cs
+++ senior_thesis/Assets/Scripts/EnemyPatrol.cs
@@ -5,23 +5,58 @@ public class EnemyPatrol : MonoBehaviour
     [SerializeField] private Transform leftEdge;
     [SerializeField] private Transform rightEdge;
     
-    //[SerializeField] private float speed = 1f;
-    [SerializeField] private GameObject enemy;
-    private Rigidbody2D _enemyRigidBody;
+    [SerializeField] private float speed = 1f;
+    
+    private Vector3 _initScale;
+    private bool _movingLeft;
 
-    void Start()
+    void Awake()
     {
-        _enemyRigidBody = enemy.GetComponent<Rigidbody2D>();
+        //storing scale of enemy
+        _initScale = transform.localScale;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //move to the left as long as enemy pos is >= leftEdge
+        if (_movingLeft)
+        {
+            if (transform.position.x >= leftEdge.position.x)
+            {
+                MoveInDirection(-1);   
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+        //move to the right as long as enemy pos is <= rightEdge
+        else
+        {
+            if (transform.position.x <= rightEdge.position.x)
+            {
+                MoveInDirection(1);
+            }
+            else
+            {
+               DirectionChange(); 
+            }
+        }
+    }
+
+    //reverses value of _movingLeft to change direction
+    private void DirectionChange()
+    {
+        _movingLeft =  !_movingLeft;
     }
     
     private void MoveInDirection(int direction)
     {
+        //enemy faces this direction--x can be negative or positive
+        //transform.localScale = new Vector3(Mathf.Abs(_initScale.x) * direction, _initScale.y, _initScale.z);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveInDirection(1);
+        //enemy moves in this direction
+        transform.position = new Vector3(transform.position.x + Time.deltaTime * direction * speed, transform.position.y, transform.position.z);
     }
 }
