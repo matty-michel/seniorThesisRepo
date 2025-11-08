c38b8cbd34e136e8e0c40e0d7a050b6e6527f560
matty-michel <mmichel@carthage.edu>
2025-11-07 20:10:40 -0600

    created spikes that damage & slow down the player

---
+++ senior_thesis/Assets/Scripts/Spikes.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private GameObject _player;
+    private Health _playerHealth;
+    private PlayerController _playerController;
+    private float _originalSpeed;
+    private float _originalJumpForce;
+    [SerializeField] Collider2D playerCollider;
+
+    void Awake()
+    {
+        //getting player object
+        _player = GameObject.FindGameObjectWithTag("Player");
+        //getting health & control scripts
+        _playerHealth = _player.GetComponent<Health>();
+        _playerController = _player.GetComponent<PlayerController>();
+        //setting original speed & jump force to reset in collision exit
+        _originalSpeed = _playerController.speed;
+        _originalJumpForce = _playerController.jumpForce;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //player takes damage & is slowed down when they hit spikes
+        if (collision.collider == playerCollider)
+        {
+            _playerHealth.TakeDamage(1);
+            _playerController.speed *= 0.5f;
+            _playerController.jumpForce *= 0.5f;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        //player speed & jump force are reset when they are no longer hitting spikes
+        if (collision.collider == playerCollider)
+        {
+            _playerController.speed = _originalSpeed;
+            _playerController.jumpForce = _originalJumpForce;
+        }
+    }
+}
