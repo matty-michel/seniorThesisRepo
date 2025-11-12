cff5a67c834af3f5230caf409ed955212b2db29d
matty-michel <mmichel@carthage.edu>
2025-11-12 15:39:37 -0600

    moving platform becomes parent of player to update movement
    - player's transform automatically changes with the platform's transform

--- senior_thesis/Assets/Scripts/Other/UpdatePlayerPos.cs
+++ senior_thesis/Assets/Scripts/Other/UpdatePlayerPos.cs
@@ -2,29 +2,11 @@ using UnityEngine;
 
 public class UpdatePlayerPos : MonoBehaviour
 {
-    private bool _isOnMovingPlatform;
-    private GameObject _movingPlatform;
-    private Rigidbody2D _playerRigidbody;
-
-    void Awake()
-    {
-        _playerRigidbody = GetComponent<Rigidbody2D>();
-    }
-    void FixedUpdate()
-    {
-        if (_isOnMovingPlatform)
-        {
-            _playerRigidbody.MovePosition(_movingPlatform.transform.position);
-            //transform.position.x = _movingPlatform.transform.position;
-        }
-    }
-    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("MovingPlatform"))
         {
-            _movingPlatform = other.gameObject;
-            _isOnMovingPlatform = true;
+            transform.parent = other.gameObject.transform;
         }
     }
 
@@ -32,7 +14,7 @@ public class UpdatePlayerPos : MonoBehaviour
     {
         if (other.gameObject.CompareTag("MovingPlatform"))
         {
-            _isOnMovingPlatform = false;
+            transform.parent = null;
         }
     }
 }
