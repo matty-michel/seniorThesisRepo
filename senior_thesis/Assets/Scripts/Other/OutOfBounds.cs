3162f29fc85753efb94c04da4569f4c6e0ec0154
matty-michel <mmichel@carthage.edu>
2026-03-23 21:39:46 -0500

    player & enemies die when they fall out of bounds

---
+++ senior_thesis/Assets/Scripts/Other/OutOfBounds.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private Health _playerHealth;
+    void Start()
+    {
+        _playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //killing player when they fall out of bounds
+        if (other.CompareTag("Player"))
+        {
+            _playerHealth._dead = true;
+        }
+        //destroying enemies that fall out of bounds
+        else if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
