e06d37350c0ca40fdd0d5f4e7e08bba86d78837a
matty-michel <mmichel@carthage.edu>
2026-03-25 21:48:01 -0500

    worked on level 1 & added music
    - also split control script into two

--- senior_thesis/Assets/Scripts/UI/Control.cs
+++ senior_thesis/Assets/Scripts/UI/Control.cs
@@ -3,31 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Control : MonoBehaviour
 {
-    [SerializeField] private GameObject deathMenu;
-    [SerializeField] private GameObject pauseMenu;
-    [SerializeField] private GameObject healthBar;
-    [SerializeField] private GameObject player;
-    private Health _playerHealth;
-
-    void Start()
-    {
-        //getting player health
-        _playerHealth = player.GetComponent<Health>();
-    }
-
-    void Update()
-    {
-        //activating game over screen when player dies
-        if (_playerHealth._dead)
-        {
-            GameOver();
-        }
-        //activating pause menu when player presses 'P'
-        else if(Input.GetKeyDown(KeyCode.P))
-        {
-            PauseGame();
-        }
-    }
     public void LoadScene(string scene)
     {
         //loading a scene
@@ -38,41 +13,5 @@ public class Control : MonoBehaviour
     {
         //quitting game
         Application.Quit();
-    }
-
-    private void GameOver()
-    {
-        //activating game over screen
-        deathMenu.SetActive(true);
-    }
-
-    public void Respawn()
-    {
-        //reloading current level
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        //deactivating game over screen
-        deathMenu.SetActive(false);
-    }
-    
-    private void PauseGame()
-    {
-        //freezing scene
-        Time.timeScale = 0;
-        
-        //opening pause menu
-        pauseMenu.SetActive(true);
-        //hiding health bar
-        healthBar.SetActive(false);
-    }
-
-    public void ResumeGame()
-    {
-        //unfreezing scene
-        Time.timeScale = 1;
-        
-        //closing pause menu
-        pauseMenu.SetActive(false);
-        //showing health bar
-        healthBar.SetActive(true);
     }
 }
---
+++ senior_thesis/Assets/Scripts/UI/ControlMenus.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenus : MonoBehaviour
+{
+    [SerializeField] private GameObject deathMenu;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject healthBar;
+    [SerializeField] private GameObject player;
+    private Health _playerHealth;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //getting player health
+        _playerHealth = player.GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //activating game over screen when player dies
+        if (_playerHealth._dead)
+        {
+            GameOver();
+        }
+        //activating pause menu when player presses 'P'
+        else if(Input.GetKeyDown(KeyCode.P))
+        {
+            PauseGame();
+        }
+    }
+    
+    private void GameOver()
+    {
+        //activating game over screen
+        deathMenu.SetActive(true);
+    }
+
+    public void Respawn()
+    {
+        //reloading current level
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //deactivating game over screen
+        deathMenu.SetActive(false);
+    }
+    
+    private void PauseGame()
+    {
+        //freezing scene
+        Time.timeScale = 0;
+        
+        //opening pause menu
+        pauseMenu.SetActive(true);
+        //hiding health bar
+        healthBar.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        //unfreezing scene
+        Time.timeScale = 1;
+        
+        //closing pause menu
+        pauseMenu.SetActive(false);
+        //showing health bar
+        healthBar.SetActive(true);
+    }
+}
