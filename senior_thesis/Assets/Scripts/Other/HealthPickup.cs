f5680c099a113d66ad37eddd2ae6f77a48d3b98f
matty-michel <mmichel@carthage.edu>
2026-04-01 21:15:56 -0500

    worked on animations and sound effects

--- senior_thesis/Assets/Scripts/Other/HealthPickup.cs
+++ senior_thesis/Assets/Scripts/Other/HealthPickup.cs
@@ -6,24 +6,27 @@ public class HealthPickup : MonoBehaviour
     [SerializeField] Collider2D playerCollider;
     [SerializeField] private int healthAmount;
     [SerializeField] private AudioClip gotPickupSound;
-    
+    private Animator _animator;
     private Health _playerHealth;
 
     void Awake()
     {
         _playerHealth = GameObject.Find("Player").GetComponent<Health>();
+        _animator = GetComponent<Animator>();
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider == playerCollider)
         {
-            Debug.Log("added " + healthAmount);
-            Destroy(gameObject);
-            _playerHealth.currentHealth += healthAmount;
-            
+            //set animator parameter
+            _animator.SetBool("Collected", true);
             //play got pickup sound
             SoundManager.Instance.PlayAudio(gotPickupSound);
+            //destroy pickup
+            gameObject.SetActive(false);
+            //add to player's health
+            _playerHealth.currentHealth += healthAmount;
         }
     }
 }
--- senior_thesis/Assets/Scripts/Traps/Trampoline.cs
+++ senior_thesis/Assets/Scripts/Traps/Trampoline.cs
@@ -4,13 +4,16 @@ public class Trampoline : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
     [SerializeField] private GameObject player;
+    [SerializeField] private AudioClip jumpSound;
     private PlayerController _playerController;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     private void Start()
     {
         _playerController = player.GetComponent<PlayerController>();
         _rigidbody = player.GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -21,6 +24,19 @@ public class Trampoline : MonoBehaviour
             _playerController.isOnGround = false;
             //add upward force to player
             _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //setting animation parameter
+            _animator.SetBool("Jump", true);
+            //playing jump sound
+            SoundManager.Instance.PlayAudio(jumpSound);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            //setting animation parameter
+            _animator.SetBool("Jump", false);
         }
     }
 }
