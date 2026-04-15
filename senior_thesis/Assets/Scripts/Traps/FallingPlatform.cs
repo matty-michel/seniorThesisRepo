f6f25faa6f288b7ee8970984cc607bb75267c46d
matty-michel <mmichel@carthage.edu>
2026-04-15 16:20:04 -0500

    implemented more traps
    - added saws
    - added falling platforms

---
+++ senior_thesis/Assets/Scripts/Traps/FallingPlatform.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour
+{
+    [SerializeField] float timeBeforeFall;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //wait a few seconds before dropping platform
+            StartCoroutine(Fall());
+        }
+    }
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(timeBeforeFall);
+        
+        //platform falls
+        _rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        _rigidbody.AddForce(Vector2.down * 2.5f, ForceMode2D.Impulse);
+        
+        //destroy platform
+        Destroy(gameObject, timeBeforeFall);
+    }
+}
