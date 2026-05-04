e97d6aac488cf9a9e4c2e61001f260a7a22715ec
matty-michel <mmichel@carthage.edu>
2026-05-03 22:35:05 -0500

    added parallax background
    - background sections move at different speeds with player

---
+++ senior_thesis/Assets/Scripts/UI/RepeatBackground.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private float _startPos;
+    private float _length;
+    
+    private GameObject _player;
+    
+    private float _distance;
+    private float _movement;
+    
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _startPos = transform.position.x;
+        _length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    
+    void FixedUpdate()
+    {
+        //calculating background movement based on camera movement -- speed between 1 & 0, 1 being stationary & 0 moving with cam
+        _distance = _player.transform.position.x * speed;
+        _movement = _player.transform.position.x * (1 - speed);
+        
+        //adding distance to position
+        transform.position = new Vector3(_startPos + _distance, transform.position.y, transform.position.z);
+
+        //adjust background position if camera reaches the edge
+        if (_movement > _startPos + _length)
+        {
+            _startPos += _length;
+        }
+        else if (_movement < _startPos - _length)
+        {
+            _startPos -= _length;
+        }
+    }
+}
